43e7b0199d24dc7acb4b05a6531daf304dd29084
edwin141999 <183433@ids.upchiapas.edu.mx>
2022-05-30 19:39:30 -0500

    terminado

---
+++ index.js
@@ -0,0 +1,24 @@
+// EXPRESS
+const express = require('express');
+const Pokedex = require('pokeapi-js-wrapper');
+const P = new Pokedex.Pokedex();
+const app = express();
+const port = 3000;
+
+// ROUTER
+app.get('/:pokemon', async (req, res) => {
+  P.getPokemonByName(req.params.pokemon)
+    .then(response => {
+      const data = {
+        name: response.name,
+        id: response.id,
+        sprite: response.sprites.front_default,
+      }
+      res.send(data);
+    })
+});
+
+// SERVER
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
